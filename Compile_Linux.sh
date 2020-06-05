294baa7574d392992736227110118c7176eba5de
xGabbro <sergabbro@gmail.com>
2020-06-05 12:03:57 +0200

    added windows icon & finished compile scripting

--- Compile_Linux.sh
+++ Compile_Linux.sh
@@ -1,2 +1,14 @@
 #! /bin/bash
-g++ main.c lib/xsocket/xsocket.c lib/utils/utils.c -o LIMPI-Linux -Ilib/xsocket -Ilib/utils
+rm -rf "build/"
+rm -rf "release/"
+
+mkdir "build"
+mkdir "release"
+
+echo "Sto compilando il programma con le librerie..."
+g++ -c main.c -o build/LIMPI-Windows.o -Ilib/xsocket -Ilib/utils
+g++ -c lib/xsocket/xsocket.c -o build/xsocket.o
+g++ -c lib/utils/utils.c -o build/utils.o
+
+echo "Sto compilando tutti i file oggetto..."
+g++ build/LIMPI-Windows.o build/xsocket.o build/utils.o -o release/LIMPI-Linux
--- Compile_Windows.bat
+++ Compile_Windows.bat
@@ -1,3 +1,20 @@
 @echo off
-g++ main.c lib/xsocket/xsocket.c lib/utils/utils.c -o LIMPI-Windows.exe -Ilib/xsocket -Ilib/utils -lws2_32 
-pause
+
+if exist "build\" rd /s /q "build"
+if exist "release\" rd /s /q "release"
+
+if not exist "build\" mkdir "build"
+if not exist "release\" mkdir "release"
+
+echo "Sto compilando il programma con le librerie..."
+g++ -c main.c -o build\LIMPI-Windows.o -Ilib\xsocket -Ilib\utils
+g++ -c lib\xsocket\xsocket.c -o build\xsocket.o
+g++ -c lib\utils\utils.c -o build\utils.o
+
+echo "Sto compilando le risorse..."
+windres resources\icon.rc build\icon.o
+
+echo "Sto compilando tutti i file oggetto..."
+g++ build\LIMPI-Windows.o build\xsocket.o build\utils.o build\icon.o -o release\LIMPI-Windows.exe -lws2_32
+
+PAUSE
